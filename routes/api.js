bec118c72796fd6e72ff471f9658cd57fa6635cb
Santiago Olguín <SantoII1735@gmail.com>
2024-10-04 19:59:17 +0000

    added /api/solve route

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,20 @@ module.exports = function (app) {
     
   app.route('/api/solve')
     .post((req, res) => {
+      let puzzleString = req.body.puzzle;
 
+      if (!puzzleString) {
+        res.json({ error: 'Required field missing' });
+        return;
+      } else if (!solver.isValidCharacters(puzzleString)) {
+        res.json({ error: 'Invalid characters in puzzle' });
+        return;
+      } else if (!solver.isValidLength(puzzleString)) {
+        res.json({ error: 'Expected puzzle to be 81 characters long' })
+        return;
+      }
+      
+      let solution = solver.solve(puzzleString);
+      res.json({solution});
     });
 };
