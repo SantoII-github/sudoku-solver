871e9db7aa2951d4e004cc3bb17ab95ff1078f4e
Santiago Olguín <SantoII1735@gmail.com>
2024-10-04 14:14:52 +0000

    separated validation functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,8 +1,16 @@
 class SudokuSolver {
 
-  validate(puzzleString) {
+  isValidLength(puzzleString) {
+    return puzzleString.length === 81;
+  }
+
+  isValidCharacters(puzzleString) {
     const regex = /^[1-9.]+$/;
-    return (puzzleString.length === 81 && regex.test(puzzleString))
+    return regex.test(puzzleString);
+  }
+
+  validate(puzzleString) {
+    return ( this.isValidLength(puzzleString) && this.isValidCharacters(puzzleString) );
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
