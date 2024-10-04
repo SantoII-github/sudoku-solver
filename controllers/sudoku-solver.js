f4bcbd530ed986d7bbbf354f0e4adb06c048643a
Santiago Olguín <SantoII1735@gmail.com>
2024-10-04 14:42:07 +0000

    added conversion functions to and from array

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -13,6 +13,19 @@ class SudokuSolver {
     return ( this.isValidLength(puzzleString) && this.isValidCharacters(puzzleString) );
   }
 
+  stringToArray(puzzleString) {
+    let puzzleArray = [];
+    for (let i = 0; i < 81; i += 9) {
+      const row = puzzleString.slice(i, i + 9).split('');
+      puzzleArray.push(row);
+    }
+    return puzzleArray;
+  }
+
+  arrayToString(puzzleArray) {
+    return puzzleArray.flat().join('');
+  }
+
   checkRowPlacement(puzzleString, row, column, value) {
 
   }
