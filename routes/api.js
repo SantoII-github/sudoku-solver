aa1b8baf02cca7749275e146729a35edf30bb297
Santiago Olguín <SantoII1735@gmail.com>
2024-10-05 15:44:50 +0000

    passed all tests

--- routes/api.js
+++ routes/api.js
@@ -12,9 +12,10 @@ module.exports = function (app) {
       let value = req.body.value;
       let coordinate = req.body.coordinate;
       let row, column;
-      let regex = /^[A-I][1-9]$/;
+      let coordRegex = /^[A-I][1-9]$/;
+      let valueRegex = /^[1-9]$/
 
-      if (!req.body.puzzleString || !req.body.value || !req.body.coordinate) {
+      if (!req.body.puzzle || !req.body.value || !req.body.coordinate) {
         res.json({ error: 'Required field(s) missing' });
         return;
       } else if (!solver.isValidCharacters(puzzleString)) {
@@ -25,35 +26,37 @@ module.exports = function (app) {
         return;
       }
 
-
-      if (regex.test(coordinate)) {
-        row = coordinate[0].toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
-        column = coordinate[1] - 1;
-      } else {
+      if (!valueRegex.test(value)) {
+        res.json({ error: 'Invalid value'})
+        return;
+      } else if (!coordRegex.test(coordinate)) {
         res.json({ "error": "Invalid coordinate" })
         return;
+      } else {
+        row = coordinate[0].toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+        column = coordinate[1] - 1;
       }
 
       let puzzleGrid = solver.stringToArray(puzzleString);
-
-      if (!solver.checkColPlacement(puzzleGrid, row, column, value)) {
-        res.json({
-          valid: false,
-          conflict: "column"
-        })
-      } else if (!solver.checkRowPlacement(puzzleGrid, row, column, value)) {
-        res.json({
-          valid: false,
-          conflict: "row"
-        })
-      } else if (!solver.checkRegionPlacement(puzzleGrid, row, column, value)) {
-        res.json({
-          valid: false,
-          conflict: "region"
-        })
-      } else {
-        res.json({ valid: true })
+      let conflict = [];
+      if (puzzleGrid[row][column] == value) {
+        puzzleGrid[row][column] = ".";
       }
+      if (!solver.checkRowPlacement(puzzleGrid, row, column, value)) {
+          conflict.push("row")
+      }
+      if (!solver.checkColPlacement(puzzleGrid, row, column, value)) {
+        conflict.push("column")
+      } 
+      if (!solver.checkRegionPlacement(puzzleGrid, row, column, value)) {
+        conflict.push("region")
+      }
+
+      if (conflict.length === 0) {
+        res.json({ valid: true })
+      } else {
+        res.json({ valid: false, conflict})
+      } 
 
     });
     
